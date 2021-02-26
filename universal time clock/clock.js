4f109396ba6b795ce03a9cc3c7aea9ef84fd56fc
Lucaseeyou <68516521+lucaseeyou2@users.noreply.github.com>
2021-02-25 20:21:37 -0500

    Update clock.js

--- universal time clock/clock.js
+++ universal time clock/clock.js
@@ -1,16 +1,20 @@
 "use strict";
+// Get the document element
 let dc = document; 
 
 setInterval( ()=>{
+    // Create a new Date each time to get the current date
     let dt = new Date(); 
     let a = dc.getElementById("utc"); 
     let b = dt.getUTCHours();
     let c = dt.getUTCMinutes();
     let d = dt.getUTCSeconds(); 
+    // If the values are single digits, then add a 0. So if there is 1 it will return 01. 
     if(d.toString().length == 1){d = "0" + d;};
     if(c.toString().length == 1){c = "0" + c;};
     a.innerHTML = `${b}:${c}:${d}`; 
 
+    // If the value suparsses 23 hours 
     if (b == 23){b.toString().replace("23", "0");};
 
     let j = dc.getElementById("+1"); 
@@ -18,6 +22,7 @@ setInterval( ()=>{
 
     let p = dc.getElementsByClassName("-5"); 
     for (let i = 0; i < p.length; i++){p[i].innerHTML=`${b-(b-18)}:${c}:${d}`;};
+    // For each element in this class, apply a date with a time deviation 
     let o = dc.getElementsByClassName("-6"); 
     for (let i = 0; i < o.length; i++){o[i].innerHTML=`${b-(b-17)}:${c}:${d}`;};
     let m = dc.getElementsByClassName("-7"); 
@@ -29,6 +34,7 @@ setInterval( ()=>{
     let l = dc.getElementsByClassName("-10"); 
     for (let i = 0; i < l.length; i++){l[i].innerHTML=`${b-(b-12)}:${c}:${d}`;};
 
+    //  Local time
     let f = dc.getElementById("lc"); 
     let g = dt.getHours(); 
     let h = dt.getMinutes(); 
